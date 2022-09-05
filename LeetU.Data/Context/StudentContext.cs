73435181fd60f4c3c42339ca3c6cb651e64c8325
RossMill1976 <rossajmiller76@gmail.com>
2022-09-05 20:31:26 +0100

    Removed connection string

--- LeetU.Data/Context/StudentContext.cs
+++ LeetU.Data/Context/StudentContext.cs
@@ -21,11 +21,6 @@ namespace LeetU.Data.Context
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlite("DataSource=c:\\db\\student.db");
-            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
